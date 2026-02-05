af771cb59cff22db157ed60c573b201dfe882737
KucielKrzysztof <krzysztof.kuciel1@gmail.com>
2026-02-05 17:55:49 +0100

    feat: add portfolio project disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistGate } from "redux-persist/integration/react";
 import { HelmetProvider } from "react-helmet-async";
+import ProjectDisclaimer from "./ui/ProjectDisclaimer";
 
 const router = createBrowserRouter(routes);
 
@@ -23,6 +24,7 @@ function App(): React.JSX.Element {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
+        <ProjectDisclaimer />
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <StyledToaster />
